b88b02b06e9a0dd1722bbf1cdbc1203e198d39dc
Vagebond <2057667293@qq.com>
2020-08-07 10:18:16 +0800

    创建ssm工程

---
+++ ssm/src/test/java/cn/itcast/test/TestSpring.java
@@ -0,0 +1,25 @@
+package cn.itcast.test;
+
+import cn.itcast.service.AccountService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Vagaband
+ * @date 2020/5/26 8:57
+ */
+public class TestSpring {
+
+    @Test
+    public void run1(){
+        // 加载配置文件
+        ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:applicationContextSSM.xml");
+        // 获取对象
+        AccountService as = (AccountService) ac.getBean("accountService");
+        // 调用方法
+        as.findAll();
+    }
+
+}
+
